76641811bd853276f870e8f5b94a9e7a26eb0632
Matheus <matheuszago134@gmail.com>
2025-03-19 16:07:32 -0300

    Add search by filter

--- src/main/java/com/matheusluizago/libraryapi/service/AuthorService.java
+++ src/main/java/com/matheusluizago/libraryapi/service/AuthorService.java
@@ -4,6 +4,7 @@ import com.matheusluizago.libraryapi.model.Author;
 import com.matheusluizago.libraryapi.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,5 +27,17 @@ public class AuthorService {
 
     public void delete(Author author) {
         repository.delete(author);
+    }
+
+    public List<Author> search(String name, String nationality){
+        if(name != null && nationality != null){
+            return repository.findByNameAndNationality(name, nationality);
+        } else if(name != null) {
+            return repository.findByName(name);
+        } else if(nationality != null){
+            return repository.findByNationality(nationality);
+        } else {
+            return repository.findAll();
+        }
     }
 }
