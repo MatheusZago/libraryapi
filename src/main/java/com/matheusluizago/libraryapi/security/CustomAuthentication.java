c4ad93580c4666adeb45fab2c5c379d5d7c0b3a4
Matheus <matheuszago134@gmail.com>
2025-05-16 15:13:55 -0300

    Add custom authentication

---
+++ src/main/java/com/matheusluizago/libraryapi/security/CustomAuthentication.java
@@ -0,0 +1,63 @@
+package com.matheusluizago.libraryapi.security;
+
+import com.matheusluizago.libraryapi.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomAuthentication implements Authentication {
+
+    private final User user;
+
+    public CustomAuthentication(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //Transform roles in authorities
+        return this.user
+                .getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return user;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return user.getLogin();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
