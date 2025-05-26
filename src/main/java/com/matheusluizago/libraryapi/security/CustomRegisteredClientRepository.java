0f792f44032e078cf397ba3469d1fc49319e0e31
Matheus <matheuszago134@gmail.com>
2025-05-26 13:16:22 -0300

    Add global configuration for token and clients

--- src/main/java/com/matheusluizago/libraryapi/security/CustomRegisteredClientRepository.java
+++ src/main/java/com/matheusluizago/libraryapi/security/CustomRegisteredClientRepository.java
@@ -5,15 +5,21 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 @Component //This will be used to return the registered client, for the auth server see if the client is correctly registered
 public class CustomRegisteredClientRepository  implements RegisteredClientRepository {
 
     private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
 
-    public CustomRegisteredClientRepository(ClientService clientService) {
+    public CustomRegisteredClientRepository(ClientService clientService, TokenSettings tokenSettings, ClientSettings clientSettings) {
         this.clientService = clientService;
+        this.tokenSettings = tokenSettings;
+        this.clientSettings = clientSettings;
     }
 
     @Override
@@ -40,6 +46,8 @@ public class CustomRegisteredClientRepository  implements RegisteredClientReposi
                      .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                      .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                      .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                     .tokenSettings(tokenSettings)
+                     .clientSettings(clientSettings)
                     .build();
         } else {
             return null;
